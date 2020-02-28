ed9b5197ce86cad68147ed0247897dc04e261ee6
Dylan Curran <c00217112@itcarlow.ie>
2020-02-28 18:24:11 +0000

    adding base audio and updated options screen

--- ARGO/ARGO/Game.cpp
+++ ARGO/ARGO/Game.cpp
@@ -1,7 +1,7 @@
 #include "Game.h"
 
 
-GameState Game::m_currentMode{ GameState::splash};
+GameState Game::m_currentMode{ GameState::mainMenu};
 EntityManager manager;
 auto& newPlayer(manager.addEntity("player"));
 auto& flag(manager.addEntity("goal"));
